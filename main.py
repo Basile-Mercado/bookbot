b0f47ceff3838a1a93c5691ed8da86210c323c9d
Basile-Mercado <basile.mercado@gmail.com>
2025-08-06 18:39:08 +0200

    add function counting the amount of words in a book

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def get_book_text(filepath):
     with open(filepath) as f:
-        file_contents = f.read()
-    print(file_contents)        
+        file_contents = f.read()      
     return file_contents
 
+def get_number_of_words(text):
+    list_of_words = text.split()
+    count = 0
+    for i in range(0, len(list_of_words)):
+        count += 1
+    return count
+
 def main():
-    get_book_text('./books/frankenstein.txt')
-    
+    text = get_book_text('./books/frankenstein.txt')
+    num_words = get_number_of_words(text)
+    print(f"{num_words} words found in the document")
 main()
