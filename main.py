a6cf77d141fca002eb8af6be67ac84f3065c121c
Basile-Mercado <basile.mercado@gmail.com>
2025-08-06 19:20:45 +0200

    add function to get amount of each characters in a book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,6 @@ def main():
     text = get_book_text('./books/frankenstein.txt')
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    
+    char_dic = get_num_char(text)
+    print(char_dic)
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,13 @@
 def get_num_words(text):
     list_of_words = text.split()
-    count = 0
-    for i in range(0, len(list_of_words)):
-        count += 1
-    return count
+    return len(list_of_words)
+
+def get_num_char(text):
+    char_dict = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char_dict:
+            char_dict[lowered] += 1
+        else:
+            char_dict[lowered] = 1
+    return char_dict
