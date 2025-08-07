913935244901a240d36322fed23c36e089b3b2f8
Basile-Mercado <basile.mercado@gmail.com>
2025-08-07 14:09:51 +0200

    add get report function

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, sort_char_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()      
     return file_contents
 
-def main():
+def get_report():
+    print("""============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...""")
     text = get_book_text('./books/frankenstein.txt')
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    char_dic = get_num_char(text)
-    print(char_dic)
+    print(f"""----------- Word Count ----------\nFound {num_words} total words""")
+    char_dict = get_num_char(text)
+    sorted_list = sort_char_dict(char_dict)
+    for sorted in sorted_list:
+        for s in sorted:
+            print(f"{s}: {sorted[s]}")
+    
+def main():
+    get_report()
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def sort_on(items):
+    for i in items:
+        return items[i]
+
 def get_num_words(text):
     list_of_words = text.split()
     return len(list_of_words)
@@ -11,3 +15,11 @@ def get_num_char(text):
         else:
             char_dict[lowered] = 1
     return char_dict
+
+def sort_char_dict(char_dict):
+    list_of_dict = []
+    for c in char_dict:
+        if c.isalpha():
+            list_of_dict.append({c: char_dict[c]})   
+    list_of_dict.sort(reverse=True, key=sort_on)
+    return list_of_dict     
